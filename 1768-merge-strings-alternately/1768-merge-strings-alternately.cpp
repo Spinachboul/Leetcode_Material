11fc688d7197f667620f4e096f556702043fc4ee
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-02-10 18:25:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -5,24 +5,13 @@ public:
         int m = word2.size();
         string res = "";
         int i=0, j=0;
-        while(i<n && j<m){
+        while(i<n || j<m){
             res += word1[i];
             res += word2[j];
             i++;j++;
         }
-        if(n>m){
-            for(int k=i ; k<n ; k++){
-                res += word1[k];
-            }
-
-        }
-        else if(n<m){
-            for(int k=j ; k<m ; k++){
-                res += word2[k];
-            }
-        }
         return res;
-
+        
     }
         
 };
