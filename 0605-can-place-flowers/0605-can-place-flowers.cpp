2d599bf9857e21444e0e6e998010063d328beee2
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-02-10 18:49:05 +0530

    Time: 0 ms (100%), Space: 24.2 MB (41.29%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -5,17 +5,17 @@ public:
 
         int count = 0;
         for(int i=0 ; i<nn ; i++){
-            if(i < nn-1 && i>0){
-                if(flowerbed[i] == 0){
-                    if(flowerbed[i+1] ==0 && flowerbed[i-1]==0){
-                        flowerbed[i] = 1;
-                        count++;
-
-                    }
+            if(flowerbed[i] == 0){
+                bool left = (i==0) || (flowerbed[i-1] == 0);
+                bool right = (i==nn-1) || (flowerbed[i+1] == 0);
+                if(left && right){
+                    flowerbed[i] = 1;
+                    count++;
                 }
             }
 
         }
+        
         return count >= n;
 
         
