5a3ca8c6c076c0bb1f75b00af1811ed209e40702
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-02-13 09:12:45 +0530

    Time: 91 ms (81.17%), Space: 91.4 MB (90.45%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -6,15 +6,15 @@ public:
         priority_queue<ll, vector<ll>, greater<ll>> pq (nums.begin() , nums.end());
         int operations = 0;
         while(pq.size() > 1 && pq.top() < k){
-            int x = pq.top();
+            ll x = pq.top();
             pq.pop();
-            int y = pq.top();
+            ll y = pq.top();
             pq.pop();
             ll new_val = min(x, y) * 2 + y;
             pq.push(new_val);
             operations++;
         }
-        return (pq.top() >=k ) ? operations : -1;
+        return operations;
 
 
         
