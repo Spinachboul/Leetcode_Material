f2b733eaf54d79dc6fe82ac9c30600219add0bf7
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-02-20 12:06:56 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.18%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,24 +1,29 @@
 class Solution {
 vector<string> mp;
-private:
-    void generate(int n, int index, string current){
-        if(index == n){
-            mp.push_back(current);
-            return;
-        }
+// private:
+//     void generate(int n, int index, string current){
+//         if(index == n){
+//             mp.push_back(current);
+//             return;
+//         }
 
-        generate(n, index+1, current + "0");
-        generate(n, index+1, current + "1");
-    }
+//         generate(n, index+1, current + "0");
+//         generate(n, index+1, current + "1");
+//     }
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        set<string> st (nums.begin() , nums.end());
-        generate(nums.size(), 0, "");
-        for(int i=0 ; i<(int)mp.size() ; i++){
-            if(st.find(mp[i]) == st.end()){
-                return mp[i];
+        unordered_set<int> ints;
+        for(auto& it: nums){
+            ints.insert(stoi(it, 0, 2));
+        }
+
+        for(int i=0 ; i<=nums.size() ; i++){
+            if(ints.find(i) == ints.end()){
+                string ans = bitset<16>(i).to_string();
+                return ans.substr(16-nums.size());
             }
         }
-        return "-1";
+        return "";
+
     }
 };
