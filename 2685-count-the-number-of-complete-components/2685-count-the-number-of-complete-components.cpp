616d798d86f9601bd28b2e67017a0fa7a2b73f13
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-03-22 09:39:44 +0530

    Time: 70 ms (37.01%), Space: 134.2 MB (50.81%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,59 @@
+class Solution {
+private:
+    void DFS(int src, vector<vector<int>> &graph, vector<bool> &vis, set<int> &st){
+
+        // set is used to store the component
+        vis[src] = true;
+        st.insert(src);
+        for(auto& it: graph[src]){
+            if(!vis[it]){
+                DFS(it, graph, vis, st);
+            }
+        }
+    }
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph;
+        graph.resize(n);
+        for(auto& it: edges){
+            int u = it[0];
+            int v = it[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        vector<bool> vis(n, false);
+
+        // to store the number of connected components
+        vector<vector<int>> cc;
+
+        for(int i=0 ; i<n ; i++){
+            if(!vis[i]){
+                set<int> component;
+                DFS(i, graph, vis, component);
+                
+                // check if the component formed is complete
+                int k = component.size();
+                // count the number of edges
+                int expected = k * (k-1)/2;
+                int actual = 0;
+                for(int node: component){
+                    actual += graph[node].size();
+                    
+                }
+
+                actual /= 2; // each edge is actually counted twice in an adjacency list
+
+                if(actual == expected){
+                    cc.push_back(vector<int> (component.begin() , component.end()));
+                }
+            }
+        }
+
+        return cc.size();
+
+
+
+        
+    }
+};
