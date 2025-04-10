3816642b58f17a3396faac17f8cb321618db55d4
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-04-10 13:50:50 +0530

    Time: 0 ms (100%), Space: 10.1 MB (55.56%) - LeetHub

--- 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
+++ 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
-    long long numberOfPowerfulInt(long long start, long long finish, int limit,
-                                  string s) {
-        string low = to_string(start);
-        string high = to_string(finish);
-        int n = high.size();
-        low = string(n - low.size(), '0') + low;  // align digits
-        int pre_len = n - s.size();               // prefix length
+    long long numberOfPowerfulInt(long long start, long long finish, int limit,string s) {
+       string st = to_string(start - 1);
+       string e = to_string(finish);
 
-        vector<long long> memo(n, -1);
-        function<long long(int, bool, bool)> dfs =
-            [&](int i, bool limit_low, bool limit_high) -> long long {
-            // recursive boundary
-            if (i == low.size()) {
-                return 1;
+       return calc(e, s, limit) - calc(st, s, limit);
+    }
+
+    long long calc(string x, string s, int limit){
+        // base conditions
+        if(x.size() < s.size()) return 0;
+        if(x.size() == s.size()){
+            return x>=s ? 1 : 0;
+        }
+
+        string suffix = x.substr(x.length() - s.length(), s.length());
+        long long count = 0;
+        int preLen = x.size() - s.size();
+        for(int i=0 ; i< preLen ; i++){
+            if(limit < (x[i] - '0')){
+                count += (long) pow(limit+1, preLen-i);
+                return count;
             }
-
-            if (!limit_low && !limit_high && memo[i] != -1) {
-                return memo[i];
-            }
-
-            int lo = limit_low ? low[i] - '0' : 0;
-            int hi = limit_high ? high[i] - '0' : 9;
-
-            long long res = 0;
-            if (i < pre_len) {
-                for (int digit = lo; digit <= min(hi, limit); digit++) {
-                    res += dfs(i + 1, limit_low && digit == lo,
-                               limit_high && digit == hi);
-                }
-            } else {
-                int x = s[i - pre_len] - '0';
-                if (lo <= x && x <= min(hi, limit)) {
-                    res =
-                        dfs(i + 1, limit_low && x == lo, limit_high && x == hi);
-                }
-            }
-
-            if (!limit_low && !limit_high) {
-                memo[i] = res;
-            }
-            return res;
-        };
-        return dfs(0, true, true);
+            count += (long) (x[i] - '0') * (long) pow(limit +1 , preLen - 1 - i);
+        }        
+        if(suffix >= s){
+            count++;
+        }
+        return count;
     }
 };
