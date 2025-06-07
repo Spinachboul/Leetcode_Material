d6248969393cf3d79d0f66419082e46645cdfccc
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-06-07 12:11:02 +0530

    Time: 62 ms (76.62%), Space: 37.8 MB (31.49%) - LeetHub

---
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        int n = s.size();
+        vector<stack<int>> st(26);
+        for(int i=0 ; i<s.size() ; i++){
+            if(s[i] != '*'){
+                st[s[i] - 'a'].push(i);
+            }
+            else{
+                for(int j=0 ; j<26 ; j++){
+                    if(!st[j].empty()){
+                        s[st[j].top()] = '*';
+                        st[j].pop();
+                        break;
+                    }
+                }
+            }
+        }
+        string ans;
+        for(int i=0 ; i<s.size() ; i++){
+            if(s[i] != '*'){
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+
+
+        
+    }
+};
