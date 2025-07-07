a803ff5aa7da81faaf0b9e8dda294e69475d3882
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-07-07 13:07:10 +0530

    Time: 59 ms (79.86%), Space: 75 MB (64.96%) - LeetHub

---
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        int n = events.size();
+
+        // first find max end time
+        int maxDay = 0;
+        for(int i=0 ; i<n;i++){
+            maxDay = max(maxDay, events[i][1]);
+
+        }
+        sort(events.begin() , events.end());
+        priority_queue<int, vector<int> , greater<>> pq;
+
+        int ans = 0;
+        for(int i=0, j=0; i<= maxDay; i++){
+            while(j < n && events[j][0] <= i){
+                pq.emplace(events[j][1]);
+                j++;
+            }
+            while(!pq.empty() && pq.top() < i){
+                pq.pop();
+            }
+            if(!pq.empty()){
+                pq.pop();
+                ans++;
+            }
+
+        }
+        return ans;
+        
+    }
+};
