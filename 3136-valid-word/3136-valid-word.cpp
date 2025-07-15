0859c0c8c2a1d218306cc6076507c2cfc5aefbec
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-07-15 11:39:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3136-valid-word/3136-valid-word.cpp
+++ 3136-valid-word/3136-valid-word.cpp
@@ -1,54 +1,25 @@
-from typing import List, Set
+class Solution {
+public:
+    bool isValid(string word) {
+        if(word.size() < 3) return false;
 
-"""Class to store reusable constan values."""
-class Constants:
-    vowels: Set[str] = set(['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'])
-
-"""Interface class for a rule. All rule classes must implement the is_valid method."""
-class RuleInterface:
-    def is_valid(word: str) -> bool:
-        raise("Not implemented error")
-
-"""Rule class implementing check on minimum length of a word."""
-class MinimumLengthRule(RuleInterface):
-    def is_valid(word: str) -> bool:
-        return len(word) >= 3
-
-"""Rule class implementing allowed characters in a word."""
-class CharacterRule(RuleInterface):
-    def is_valid(word: str) -> bool:
-        for char in word:
-            if char.isdigit() == False and (ord('A') <= ord(char) <= ord('z')) == False:
-                return False
-        return True
-
-"""Rule class implementing minimum number of vowels in a word."""
-class MinimumVowelRule(RuleInterface):
-    def is_valid(word: str) -> bool:
-        for char in word:
-            if char in Constants.vowels:
-                return True
-        return False
-
-"""Rule class implementing minimum number of consonants in a word."""
-class MinimumConsonantRule(RuleInterface):
-    def is_valid(word: str) -> bool:
-        for char in word:
-            if char.isdigit():
-                continue
-            elif char not in Constants.vowels:
-                return True
-        return False
-
-class Solution:
-    def isValid(self, word: str) -> bool:
-        # Define the rules to apply onto the word
-        rules: List[RuleInterface] = [MinimumLengthRule, CharacterRule, MinimumVowelRule, MinimumConsonantRule]
+        bool hasConsonant = true;
+        bool hasVowel = true;
+        for(auto& c: word){
+            if(isalpha(c)){
+                c = tolower(c);
+                if(c == 'a' || c == 'e' || c == 'e' || c == 'o' || c == 'u'){
+                    hasVowel = true;
+                }
+                else{
+                    hasConsonant = true;
+                }
+            }
+            else if(!isdigit(c)){
+                return false;
+            }
+        }
+        return hasVowel && hasConsonant;
         
-        # Evlauate each rule, one-by-one
-        for rule in rules:
-            # If any rule fails, return the word as invalid
-            if rule.is_valid(word) == False:
-                return False
-        # If all rules pass, then the word must be valid
-        return True
+    }
+};
