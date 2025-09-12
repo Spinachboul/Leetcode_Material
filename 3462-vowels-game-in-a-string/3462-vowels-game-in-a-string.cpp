71b32067183c41286f15cdd4579d14d028b12dec
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-09-12 06:12:20 +0530

    Time: 7 ms (58.96%), Space: 19.2 MB (25.75%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int countVowels(string &s){
+        int count = 0;
+        for(int i=0 ; i<s.size() ; i++){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+            s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+                count++;
+            }
+        }
+        return count;
+    }
+public:
+
+    bool doesAliceWin(string s) {
+        int count = countVowels(s);
+        if(count == 0) return false;
+        else return true;
+
+        
+    }
+};
