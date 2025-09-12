e660323c03c372501970f33006170a2e794e1a00
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-09-12 06:12:44 +0530

    Time: 11 ms (34.33%), Space: 19 MB (53.73%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -3,8 +3,7 @@ private:
     int countVowels(string &s){
         int count = 0;
         for(int i=0 ; i<s.size() ; i++){
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
-            s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
                 count++;
             }
         }
