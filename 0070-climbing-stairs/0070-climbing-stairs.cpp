855ec12e2ad80ea353c5bafc67727837731573d8
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-09-14 21:05:11 +0530

    Time: 0 ms (100%), Space: 8.3 MB (55.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    int solve(int n, unordered_map<int, int> mp){
+    int solve(int n, unordered_map<int, int> &mp){
         if(n==0 || n==1) return 1;
 
         if(mp.find(n) == mp.end()){
