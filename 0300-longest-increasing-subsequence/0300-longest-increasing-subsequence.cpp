c69a55411fbcf5386d0697902de0a582ccfca3e1
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-09-24 05:36:26 +0530

    Time: 67 ms (60.9%), Space: 14.2 MB (72.95%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,16 +4,17 @@ public:
 
         int n = nums.size();
         vector<int> dp(n, 1);
+        int mx = 1;
         for(int i=0 ; i<n ; i++){
             for(int j=0; j<i ; j++){
                 if(nums[i] > nums[j]){
                     dp[i] = max(dp[i] , dp[j] + 1);
                 }
+                mx = max(mx, dp[i]);
 
             }
         }
-        sort(dp.begin() , dp.end());
-        return dp.back();
+        return mx;
         
     }
 };
