0355d0297125972781513524c51fa267b722bbce
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-09-26 08:32:39 +0530

    Time: 31 ms (69.53%), Space: 16 MB (100%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -1,24 +1,34 @@
+const size_t BUFFER_SIZE = 0x6fafffff;
+alignas(std::max_align_t) char buffer[BUFFER_SIZE];
+size_t buffer_pos = 0;
+
+void* operator new(size_t size) {
+    constexpr std::size_t alignment = alignof(std::max_align_t);
+    size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
+    size_t total_size = size + padding;
+    char* aligned_ptr = &buffer[buffer_pos + padding];
+    buffer_pos += total_size;
+    return aligned_ptr;
+}
+
+void operator delete(void* ptr, unsigned long) {}
+void operator delete(void* ptr) {}
+void operator delete[](void* ptr) {}
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        int n = nums.size();
-        int count = 0;
-        for(int k=n-1 ; k>=2 ; k--){
-            int i=0, j=k-1;
-            while(i < j){
-                if(nums[i] + nums[j] > nums[k]){
-                    count += (j-i);
-                    j--;
+        int ans=0;
+        sort(nums.begin(), nums.end());
+        for(int i=2; i<nums.size(); i++){
+            int left=0, right=i-1;
+            while(left<right){
+                if((nums[left]+nums[right])>nums[i]) {
+                    ans+=right-left;
+                    right--;
                 }
-                else{
-                    i++;
-                }
+                else left++;
             }
-
-
         }
-        return count;
-        
+        return ans;
     }
 };
