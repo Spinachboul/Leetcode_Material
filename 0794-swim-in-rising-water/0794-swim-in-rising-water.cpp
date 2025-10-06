52f8d4d705d9fc1b5bf39dca3c6f559094825aba
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-10-06 15:52:28 +0530

    Time: 10 ms (42.73%), Space: 16.1 MB (17.89%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        priority_queue<tuple<int, int, int>, vector<tuple<int,int, int>>, greater<>> pq;
-
-        vector<pair<int, int>> directions = {
-            {0,1} , {1, 0}, {-1, 0}, {0,-1}
-        };
-
-        set<pair<int, int>> seen;
-
-        pq.push({grid[0][0] , 0, 0});
-
-        while(!pq.empty()){
-            auto [max_d, r, c] = pq.top();
-
-            pq.pop();
-
-
-            if(seen.count({r, c})) continue;
-
-            seen.insert({r, c});
-
-            if(r==m-1 && c==n-1) return max_d;
-
-            for(auto [dr, dc] : directions){
-                int nr = r + dr;
-                int nc = c + dc;
-
-                if(nr >=0 && nr < m && nc >=0 && nc < n && !seen.count({nr, nc})){
-                    int new_d = max(max_d, grid[nr][nc]);
-                    pq.push({new_d, nr, nc});
-                }
+        int m = grid.size(), n = grid[0].size();
+        vector<tuple<int,int,int>> edges;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0)
+                    edges.push_back({max(grid[i][j], grid[i-1][j]), i*n+j, (i-1)*n+j});
+                if (j > 0)
+                    edges.push_back({max(grid[i][j], grid[i][j-1]), i*n+j, i*n+j-1});
             }
         }
-        return -1;
-
         
+        sort(edges.begin(), edges.end());
+        vector<int> parent(m * n);
+        iota(parent.begin(), parent.end(), 0);
+        
+        function<int(int)> find = [&](int x) {
+            return parent[x] == x ? x : parent[x] = find(parent[x]);
+        };
+        
+        auto unite = [&](int x, int y) {
+            parent[find(x)] = find(y);
+        };
+        
+        for (auto [cost, u, v] : edges) {
+            unite(u, v);
+            if (find(0) == find(m*n-1))
+                return cost;
+        }
+        return grid[0][0];
     }
 };
