dbdab26ec315ee70a09c9bb7a372e2682fb4d96c
Mridul Jain <105979087+Spinachboul@users.noreply.github.com>
2025-10-13 08:50:13 +0530

    Time: 7 ms (19.08%), Space: 18.1 MB (19.22%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -1,30 +1,32 @@
 class Solution {
 private:
-    bool isAna(string& w1, string& w2){
-        sort(w1.begin() , w1.end());
-        sort(w2.begin() , w2.end());
-        return w1 == w2;
+    vector<int> score(string& word){
+        vector<int> res(26, 0);
+
+        for(char c : word){
+            res[c - 'a']+=1;
+        }
+        return res;
+        
     }
 public:
     vector<string> removeAnagrams(vector<string>& words) {
 
         int n = words.size();
-        vector<bool> toPick(n, false);
-        vector<string> ans;
+        vector<string> res;
+        vector<int>preScore = score(words[0]);
+        res.push_back(words[0]);
+
+
         for(int i=1 ; i<n ; i++){
-            string w1 = words[i], w2 = words[i-1];
-            if(isAna(w1, w2)){
-                toPick[i] = true;
+            vector<int> currScore = score(words[i]);
+            if(currScore != preScore){
+                res.push_back(words[i]);
+                preScore = currScore;
             }
         }
+        return res;
 
-        for(int i=0 ; i<n ; i++){
-            if(!toPick[i]){
-                ans.push_back(words[i]);
-            }
-        }
-
-        return ans;
 
     }
 };
